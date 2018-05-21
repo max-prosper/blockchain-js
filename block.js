19fc19a178881860a5ae0e48d13fa07a18433b10
Max Prosper <maxim.yaaman@gmail.com>
2018-05-21 23:11:47 +0300

    Genesis block

--- block.js
+++ block.js
@@ -13,6 +13,10 @@ class Block {
         Hash     : ${this.hash.substring(0, 10)}
         Data     : ${this.data}`;
     }
+
+    static genesis() {
+        return new this(`Genesis time`, `-----`, `f2r45-h45g`, []);
+    }
 }
 
 module.exports = Block;
