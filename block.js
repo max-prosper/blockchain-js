826b2ac9a496b89f440bce5ec351fa3ce1727b8c
Max Prosper <maxim.yaaman@gmail.com>
2018-05-21 23:37:21 +0300

    SHA256 Hash function and basic tests

--- block.js
+++ block.js
@@ -1,30 +1,36 @@
-class Block {
-    constructor(timestamp, lastHash, hash, data) {
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
-    }
+const SHA256 = require(`crypto-js/sha256`);
 
-    toString() {
-        return `Block -
+class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  toString() {
+    return `Block -
         Timestamp: ${this.timestamp}
         Last Hash: ${this.lastHash.substring(0, 10)}
         Hash     : ${this.hash.substring(0, 10)}
         Data     : ${this.data}`;
-    }
+  }
 
-    static genesis() {
-        return new this(`Genesis time`, `-----`, `f2r45-h45g`, []);
-    }
+  static genesis() {
+    return new this(`Genesis time`, `-----`, `f2r45-h45g`, []);
+  }
 
-    static mineBlock(lastBlock, data) {
-        const timestamp = Date.now();
-        const lastHash = lastBlock.hash;
-        const hash = `todo-hash`;
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
 
-        return new this(timestamp, lastHash, hash, data);
-    }
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
 }
 
 module.exports = Block;
