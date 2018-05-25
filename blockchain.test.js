b4c4bb6db9336308b90f7507305b5017e1dc28e3
Max Prosper <maxim.yaaman@gmail.com>
2018-05-25 18:21:48 +0300

    Chain replacemetn

--- blockchain.test.js
+++ blockchain.test.js
@@ -38,4 +38,18 @@ describe("Blockchain", () => {
 
     expect(bc.isValidChain(bc2.chain)).toBe(false);
   });
+
+  it(`replaces the chain with a valid chain`, () => {
+    bc2.addBlock("foo");
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).toEqual(bc2.chain);
+  });
+
+  it(`does not replace the chain with one of less than or equal to length of original`, () => {
+    bc.addBlock("foo");
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).not.toEqual(bc2.chain);
+  });
 });
