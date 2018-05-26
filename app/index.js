afd3775baf037bd9e27c6e63f2fdeec603893acb
Max Prosper <maxim.yaaman@gmail.com>
2018-05-26 23:40:42 +0300

    Added functions for handling peers messages and synchronizing the Blockchain

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Blockchain = require("../blockchain");
+const P2pServer = require("./p2p-server");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -17,7 +19,10 @@ app.post("/mine", (req, res) => {
   const block = bc.addBlock(req.body.data);
   console.log(`New block added: ${block.toString()}`);
 
+  p2pServer.syncChains();
+
   res.redirect("/blocks");
 });
 
 app.listen(PORT, () => console.log(`App is up and running on port ${PORT}`));
+p2pServer.listen();
