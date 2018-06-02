9bd9d89bab2494d3c8ff9adb0d0b4c400b345f3a
Max Prosper <maxim.yaaman@gmail.com>
2018-06-02 14:00:29 +0300

    GET and POST methods for transactions

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const Blockchain = require("../blockchain");
 const P2pServer = require("./p2p-server");
+const Wallet = require("../wallet");
+const TransactionPool = require("../wallet/transaction.pool");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -23,6 +27,16 @@ app.post("/mine", (req, res) => {
 
   res.redirect("/blocks");
 });
+
+app.get("/transactions", (rea, res) => {
+  res.json(tp.transactions);
+});
+
+app.post("/transact", (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  res.redirect("/transactions");
+})
 
 app.listen(PORT, () => console.log(`App is up and running on port ${PORT}`));
 p2pServer.listen();
